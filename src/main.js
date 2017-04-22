6912ac4d9757022344bc872ee857b2dce60857bc
wusong <651754835@qq.com>
2017-04-23 02:14:17 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import commonJs from './assets/common.js'
-import axios from 'axios'
-// mock数据
-import '../mock/index.js'
-Vue.prototype.$http = axios;
+
 Vue.config.productionTip = false
 
 //全局钩子
