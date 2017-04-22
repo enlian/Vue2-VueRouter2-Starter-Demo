293322bfd0aaff7a260b551876b310b79c4bbeb2
wusong <651754835@qq.com>
2017-04-23 02:02:34 +0800

    mock data ,index page

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <vHeader></vHeader>
-    index
+    <div v-for="x in data">
+      {{ x }}
+    </div>
     <vFooter></vFooter>
   </div>
 </template>
@@ -12,7 +14,23 @@
 
   export default {
     name: 'index',
-    components: {vHeader, vFooter}
+    components: {vHeader, vFooter},
+    created: function () {
+      var _this = this;
+      this.$http.get('/api')
+        .then(function (res) {
+            console.log(res.data.array)
+          _this.data = res.data.array
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    data(){
+      return {
+        data: null
+      }
+    }
   }
 </script>
 
